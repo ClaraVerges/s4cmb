eea65c3a165ca7535894b0c0ae2628affeae84f4
Clara <clara.verges@apc.in2p3.fr>
2018-03-12 09:42:23 +0100

    matrix formalism for squid to squid crosstalk

--- s4cmb/systematics.py
+++ s4cmb/systematics.py
@@ -252,6 +252,159 @@ def inject_crosstalk_inside_SQUID(bolo_data, squid_ids, bolo_ids, frequency,
     else:
         bolo_data[:] = tsout
 
+def get_crosstalk_matrix_SQUID_to_SQUID(ndet, squid_ids, bolo_ids,
+                                    mu=-3., sigma=1.,
+                                    squid_attenuation=100.,
+                                    seed=5438765):
+    """
+    Generate the crosstalk matrix you will use to inject_crosstalk_SQUID_to_SQUID.
+    You have to provide the list of bolometers, to which SQUID they
+    belong, and the index of bolometers within each SQUID.
+
+    Parameters
+    ----------
+    ndet : int
+        Number of detectors in the focal plane
+    squid_ids : list of strings
+        Contains the SQUID id for each bolometer.
+    bolo_ids : list of strings
+        Contains the positions of bolometers within their SQUID.
+    mu : float, optional
+        Mean of the Gaussian used to generate the level of leakage,
+        in percent. E.g. mu=1.0 means the leakage coefficients will be
+        of order 1%.
+    sigma : float, optional
+        Width of the Gaussian used to generate the level of leakage,
+        in percent. E.g. sigma=1.0 means the leakage coefficients will be
+        of order X \pm 1%.
+    squid_attenuation : int, optional
+        Value controling the attenuation due to distance between SQUIDS.
+        The idea is that crosstalk depends on the separation
+        length between SQUIDs. Bolometer $b_i$ crosstalking with bolometers
+        $b_{j}$ with leakage coefficients
+        $\alpha_{j}$ will see his timestream modified as:
+            $b_i = b_i + \dfrac{\alpha_{j}}{squid_attenuation}  b_{j}$.
+        Default is 100.
+    seed : int, optional
+        Control the random seed used to generate leakage coefficients.
+    """
+    ## Make mu and sigma unitless (user provides percentage)
+    mu = mu / 100.
+    sigma = sigma / 100.
+
+    combs = {}
+    for bolo in range(ndet):
+        sq = squid_ids[bolo]
+        if sq not in combs:
+            combs[sq] = []
+        combs[sq].append((bolo_ids[bolo], bolo))
+
+    # How much to leak from one bolometer to its neighboring channels
+    state = np.random.RandomState(seed)
+    cross_amp = state.normal(mu, sigma, ndet)
+
+    cross_matrix=np.zeros((ndet,ndet))
+
+    ## Take one squid
+    for sq1 in combs:
+        ## Take all bolometers in that SQUID
+        for ch, i in combs[sq1]:
+            ## Take a second SQUID
+            for sq2 in combs:
+                if sq2 == sq1:
+                    continue
+                ## Take all bolometers in that SQUID
+                for ch2, j in combs[sq2]:
+                    cross_matrix[i][j] = cross_amp[i]/squid_attenuation
+                    
+    return cross_matrix
+
+def inject_crosstalk_SQUID_to_SQUID(bolo_data, squid_ids, bolo_ids,
+                                    mu=-3., sigma=1.,
+                                    squid_attenuation=100.,
+                                    seed=5438765, new_array=None):
+
+    """
+    Introduce leakage between bolometers from different SQUIDs.
+    You have to provide the list of bolometers, to which SQUID they
+    belong, and the index of bolometers within each SQUID.
+
+    Parameters
+    ----------
+    bolo_data : list of arrays
+        Contains the time-ordered data of bolometers.
+    squid_ids : list of strings
+        Contains the SQUID id for each bolometer.
+        Should have the same shape[0] as bolo_data.
+    bolo_ids : list of strings
+        Contains the positions of bolometers within their SQUID.
+        Should have the same shape[0] as bolo_data.
+    mu : float, optional
+        Mean of the Gaussian used to generate the level of leakage,
+        in percent. E.g. mu=1.0 means the leakage coefficients will be
+        of order 1%.
+    sigma : float, optional
+        Width of the Gaussian used to generate the level of leakage,
+        in percent. E.g. sigma=1.0 means the leakage coefficients will be
+        of order X \pm 1%.
+    squid_attenuation : int, optional
+        Value controling the attenuation due to distance between SQUIDS.
+        The idea is that crosstalk depends on the separation
+        length between SQUIDs. Bolometer $b_i$ crosstalking with bolometers
+        $b_{j}$ with leakage coefficients
+        $\alpha_{j}$ will see his timestream modified as:
+            $b_i = b_i + \dfrac{\alpha_{j}}{squid_attenuation}  b_{j}$.
+        Default is 100.
+    seed : int, optional
+        Control the random seed used to generate leakage coefficients.
+    new_array : None or bolo_data-like array, optional
+        If not None, return a new array of timestreams with the modifications.
+        Modify bolo_data directly otherwise. Default is None.
+
+    Example
+    ----------
+    >>> from s4cmb.tod import load_fake_instrument
+    >>> from s4cmb.tod import TimeOrderedDataPairDiff
+    >>> inst, scan, sky_in = load_fake_instrument(nsquid_per_mux=4)
+    >>> tod = TimeOrderedDataPairDiff(inst, scan, sky_in, CESnumber=0)
+    >>> d = np.array([tod.map2tod(det) for det in range(2 * tod.npair)])
+    >>> print(round(d[0][0], 3))
+    40.948
+
+    Inject crosstalk between bolometers in different SQUIDs
+    >>> squid_ids = inst.focal_plane.get_indices('Sq')
+    >>> bolo_ids = inst.focal_plane.bolo_index_in_squid
+    >>> inject_crosstalk_SQUID_to_SQUID(d, squid_ids, bolo_ids,
+    ...     squid_attenuation=100.)
+    >>> print(round(d[0][0], 3))
+    40.694
+
+    One can also keep the original timestreams, and return a new array
+    containing modified timestreams.
+    >>> d = np.array([tod.map2tod(det) for det in range(2 * tod.npair)])
+    >>> d_new = np.zeros_like(d)
+    >>> inject_crosstalk_SQUID_to_SQUID(d, squid_ids, bolo_ids,
+    ...     squid_attenuation=100., new_array=d_new)
+    >>> print(round(d[0][0], 3), round(d_new[0][0], 3))
+    ... #doctest: +NORMALIZE_WHITESPACE
+    40.948 40.694
+
+    """
+    tsout = 0.0 + bolo_data
+
+    cross_matrix = get_crosstalk_matrix_inside_SQUID(
+                        ndet = len(bolo_data), squid_ids = squid_ids,
+                        bolo_ids = bolo_ids, frequency = frequency,
+                        mu = mu , sigma =sigma, squid_attenuation=squid_attenuation,
+                        seed=seed)
+
+    tsout = np.dot(cross_matrix,tsout)
+
+    if new_array is not None:
+        new_array[:] = tsout
+    else:
+        bolo_data[:] = tsout
+
 def show_xtalk_amplitude(bolo_xcoord, bolo_ycoord, detector, xtalk,
                         bolo_id = None, fn_out='plot_hardware_map_test.png',
                         save_on_disk=True, display=False):
@@ -348,109 +501,6 @@ def show_xtalk_amplitude(bolo_xcoord, bolo_ycoord, detector, xtalk,
         pl.clf()
     if display:
         pl.show()
-
-def inject_crosstalk_SQUID_to_SQUID(bolo_data, squid_ids, bolo_ids,
-                                    mu=-3., sigma=1.,
-                                    squid_attenuation=100.,
-                                    seed=5438765, new_array=None):
-    """
-    Introduce leakage between bolometers from different SQUIDs.
-    You have to provide the list of bolometers, to which SQUID they
-    belong, and the index of bolometers within each SQUID.
-
-    Parameters
-    ----------
-    bolo_data : list of arrays
-        Contains the time-ordered data of bolometers.
-    squid_ids : list of strings
-        Contains the SQUID id for each bolometer.
-        Should have the same shape[0] as bolo_data.
-    bolo_ids : list of strings
-        Contains the positions of bolometers within their SQUID.
-        Should have the same shape[0] as bolo_data.
-    mu : float, optional
-        Mean of the Gaussian used to generate the level of leakage,
-        in percent. E.g. mu=1.0 means the leakage coefficients will be
-        of order 1%.
-    sigma : float, optional
-        Width of the Gaussian used to generate the level of leakage,
-        in percent. E.g. sigma=1.0 means the leakage coefficients will be
-        of order X \pm 1%.
-    squid_attenuation : int, optional
-        Value controling the attenuation due to distance between SQUIDS.
-        The idea is that crosstalk depends on the separation
-        length between SQUIDs. Bolometer $b_i$ crosstalking with bolometers
-        $b_{j}$ with leakage coefficients
-        $\alpha_{j}$ will see his timestream modified as:
-            $b_i = b_i + \dfrac{\alpha_{j}}{squid_attenuation}  b_{j}$.
-        Default is 100.
-    seed : int, optional
-        Control the random seed used to generate leakage coefficients.
-    new_array : None or bolo_data-like array, optional
-        If not None, return a new array of timestreams with the modifications.
-        Modify bolo_data directly otherwise. Default is None.
-
-    Example
-    ----------
-    >>> from s4cmb.tod import load_fake_instrument
-    >>> from s4cmb.tod import TimeOrderedDataPairDiff
-    >>> inst, scan, sky_in = load_fake_instrument(nsquid_per_mux=4)
-    >>> tod = TimeOrderedDataPairDiff(inst, scan, sky_in, CESnumber=0)
-    >>> d = np.array([tod.map2tod(det) for det in range(2 * tod.npair)])
-    >>> print(round(d[0][0], 3))
-    40.948
-
-    Inject crosstalk between bolometers in different SQUIDs
-    >>> squid_ids = inst.focal_plane.get_indices('Sq')
-    >>> bolo_ids = inst.focal_plane.bolo_index_in_squid
-    >>> inject_crosstalk_SQUID_to_SQUID(d, squid_ids, bolo_ids,
-    ...     squid_attenuation=100.)
-    >>> print(round(d[0][0], 3))
-    40.694
-
-    One can also keep the original timestreams, and return a new array
-    containing modified timestreams.
-    >>> d = np.array([tod.map2tod(det) for det in range(2 * tod.npair)])
-    >>> d_new = np.zeros_like(d)
-    >>> inject_crosstalk_SQUID_to_SQUID(d, squid_ids, bolo_ids,
-    ...     squid_attenuation=100., new_array=d_new)
-    >>> print(round(d[0][0], 3), round(d_new[0][0], 3))
-    ... #doctest: +NORMALIZE_WHITESPACE
-    40.948 40.694
-
-    """
-    ## Make mu and sigma unitless (user provides percentage)
-    mu = mu / 100.
-    sigma = sigma / 100.
-
-    combs = {}
-    for bolo in range(len(bolo_data)):
-        sq = squid_ids[bolo]
-        if sq not in combs:
-            combs[sq] = []
-        combs[sq].append((bolo_ids[bolo], bolo))
-
-    tsout = 0.0 + bolo_data
-    # How much to leak from one bolometer to its neighboring channels
-    state = np.random.RandomState(seed)
-    cross_amp = state.normal(mu, sigma, len(bolo_data))
-
-    ## Take one squid
-    for sq1 in combs:
-        ## Take all bolometers in that SQUID
-        for ch, i in combs[sq1]:
-            ## Take a second SQUID
-            for sq2 in combs:
-                if sq2 == sq1:
-                    continue
-                ## Take all bolometers in that SQUID
-                for ch2, i2 in combs[sq2]:
-                    tsout[i] += cross_amp[i2] / squid_attenuation * tsout[i2]
-
-    if new_array is not None:
-        new_array[:] = tsout
-    else:
-        bolo_data[:] = tsout
 
 def modify_beam_offsets(bolometer_xpos, bolometer_ypos,
                         mu_diffpointing=10., sigma_diffpointing=5., seed=5847):
